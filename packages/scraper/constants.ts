e4ef70b26f8edffda2da9d320cdcf6805f337ae7
Irwin Schmitt <irwinschmitt@gmail.com>
2024-07-26 15:05:57 -0300

    feat(scraper): get department ids

---
+++ packages/scraper/constants.ts
@@ -0,0 +1,3 @@
+const BASE_URL = 'https://sigaa.unb.br/sigaa/public';
+
+export const COMPONENTS_LINK = `${BASE_URL}/componentes/busca_componentes.jsf`;
--- packages/scraper/scrapers/departmentScraper.ts
+++ packages/scraper/scrapers/departmentScraper.ts
@@ -1,5 +1,6 @@
 import puppeteerSetup from '../config/puppeteer';
 import DepartmentService from '../services/departmentService';
+import { COMPONENTS_LINK } from '../constants';
 
 class DepartmentScraper {
   private departmentService: DepartmentService;
@@ -11,13 +12,36 @@ class DepartmentScraper {
   async getDepartmentIds(): Promise<number[]> {
     const page = await puppeteerSetup.newPage();
 
-    await page.goto('https://www.example.com');
+    await page.goto(COMPONENTS_LINK);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const departmentIds = await page.$$eval(
+      "select[id='form:unidades'] option",
+      (elements) => {
+        const ids: number[] = [];
+
+        for (const element of elements) {
+          const value = element.getAttribute('value');
+
+          if (value === null) {
+            continue;
+          }
+
+          const id = parseInt(value, 10);
+
+          if (id <= 0) {
+            continue;
+          }
+
+          ids.push(id);
+        }
+
+        return ids;
+      },
+    );
 
     await page.close();
 
-    return [1, 2, 3];
+    return departmentIds;
   }
 
   async scrapeDepartment(departmentId: number): Promise<void> {
@@ -33,9 +57,7 @@ class DepartmentScraper {
   async scrape(): Promise<void> {
     const departmentIds = await this.getDepartmentIds();
 
-    for (const departmentId of departmentIds) {
-      await this.scrapeDepartment(departmentId);
-    }
+    console.log(`Department ids: ${departmentIds}`);
   }
 }
 
