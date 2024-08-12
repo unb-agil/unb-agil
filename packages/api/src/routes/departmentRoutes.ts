61872cac5761160dd95f0d7135c7f4ddf7df9429
Irwin Schmitt <irwinschmitt@gmail.com>
2024-08-12 16:41:03 -0300

    chore: delete api src

--- packages/api/src/routes/departmentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { DepartmentController } from '@/controllers/departmentController';
-
-const router = Router();
-
-router.get('/', DepartmentController.getAll);
-router.post('/', DepartmentController.create);
-
-export default router;
