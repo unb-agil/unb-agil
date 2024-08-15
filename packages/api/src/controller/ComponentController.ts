4ba7ac9e474ac6857ea5c16a2b7ecc7c3f888316
Irwin Schmitt <irwinschmitt@gmail.com>
2024-08-15 18:21:03 -0300

    refactor(api): improve comp controller

--- packages/api/src/controller/ComponentController.ts
+++ packages/api/src/controller/ComponentController.ts
@@ -1,11 +1,9 @@
 import { Request } from 'express';
 import { AppDataSource } from '@/data-source';
-import Department from '@/entity/Department';
 import Component from '@/entity/Component';
 
 class ComponentController {
   private repository = AppDataSource.getRepository(Component);
-  private departmentRepository = AppDataSource.getRepository(Department);
 
   async saveSigaaIds(request: Request) {
     const sigaaIds = request.body;
@@ -14,23 +12,7 @@ class ComponentController {
   }
 
   async saveOrUpdate(request: Request) {
-    const { sigaaId } = request.params;
-    const newComponent = request.body;
-
-    const component =
-      (await this.repository.findOneBy({ sigaaId })) || new Component();
-
-    const department = await this.departmentRepository.findOneBy({
-      sigaaId: newComponent.departmentSigaaId,
-    });
-
-    component.title = newComponent.title;
-    component.type = newComponent.type;
-    component.prerequisites = newComponent.prerequisites;
-    component.corequisites = newComponent.corequisites;
-    component.equivalences = newComponent.equivalences;
-    component.department = department;
-
+    const component = request.body;
     return this.repository.save(component);
   }
 }
