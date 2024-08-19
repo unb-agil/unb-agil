2d85a3f1a8d589c2e07dcec1e77c020ed308f8f4
Irwin Schmitt <irwinschmitt@gmail.com>
2024-08-19 12:59:11 -0300

    feat(hist, #15): get dpt, program and curriculum

--- packages/academic-history/src/index.ts
+++ packages/academic-history/src/index.ts
@@ -7,12 +7,33 @@ const RELATIVE_PATH = './historico_170105342.pdf';
 const absolutePath = path.resolve(__dirname, RELATIVE_PATH);
 const file = fs.readFileSync(absolutePath);
 
-function callback(error: unknown, rows: unknown) {
+function getProgram(rows: string[][]) {
+  const [programTitle, departmentAcronym] = rows[10][1]
+    .split(' - ')[0]
+    .split('/');
+
+  return {
+    programTitle,
+    departmentAcronym,
+  };
+}
+
+function getCurriculumId(rows: string[][]) {
+  const curriculumRow = rows[13];
+  const curriculumId = curriculumRow[1].split(' ')[0];
+
+  return curriculumId;
+}
+
+function callback(error: unknown, rows: string[][]) {
   if (error) {
     return console.log(error);
   }
 
-  console.log(rows);
+  const { programTitle, departmentAcronym } = getProgram(rows);
+  const curriculumId = getCurriculumId(rows);
+
+  console.log({ departmentAcronym, programTitle, curriculumId });
 }
 
 pdf2table.parse(file, callback);
