11f7aad486d66ea6eae50590b620b146db3deb07
JoaoPCoelho <japapauloxd@hotmail.com>
2024-08-20 18:02:16 -0300

    fix(web): correcting global font #21

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
+import { Rubik } from 'next/font/google';
+
+const rubik = Rubik({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={rubik.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
