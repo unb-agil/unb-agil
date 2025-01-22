4f96367e1f4e46814de0e3879e6fa92f9f3a657a
Irwin Schmitt <irwinschmitt@gmail.com>
2025-01-22 16:50:21 -0300

    feat(#21): create hero section

---
+++ packages/web/src/components/Hero/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Card, CardContent, Typography } from '@mui/material';
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <Card elevation={0}>
+      <CardContent sx={{ padding: 3 }}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={3}
+        >
+          <Box>
+            <Typography variant="body1" fontWeight={500} gutterBottom>
+              Forme-se na UnB no menor tempo possível.
+            </Typography>
+
+            <Typography variant="body2">
+              O UnB Ágil calcula quais disciplinas você deve priorizar em cada
+              semestre de acordo com o seu histórico acadêmico. Você também pode
+              incluir disciplinas optativas em sua recomendação.
+            </Typography>
+          </Box>
+
+          <Image src="flowchart.svg" alt="Flowchart" width={68} height={68} />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
